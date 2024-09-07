3297aa51b75a6fe95d1cd437888ad675c16e4fbe
sfilips94 <sfilips94@gmail.com>
2024-09-07 15:51:06 +0200

    handle category change finished

--- js/handlers/jackets/handleCategoryChange.js
+++ js/handlers/jackets/handleCategoryChange.js
@@ -13,7 +13,7 @@ export function handleCategoryChange(jackets) {
     let selectedCategory = event.target.value;
 
     let filteredJackets = jackets.filter((jacket) => {
-      if (jacket.gender === selectedCategory) {
+      if (jacket.gender.toLowerCase() === selectedCategory.toLowerCase()) {
         return true;
       }
     });
